6fa7732c0d9ea7721bda967f52b40da6bc9a0599
helltf <Dario.Zachow5530@gmail.com>
2021-07-12 18:56:28 +0200

    Endpoint now working with database connection

--- src/main/kotlin/com/helltf/TBBackend/Controllers/ChannelController.kt
+++ src/main/kotlin/com/helltf/TBBackend/Controllers/ChannelController.kt
@@ -1,14 +1,15 @@
 package com.helltf.TBBackend.Controllers
 
 import com.helltf.TBBackend.Entities.Channel
-import com.helltf.TBBackend.Entities.Name
 import com.helltf.TBBackend.Repositories.ChannelRepository
-import com.helltf.TBBackend.Repositories.NameRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import java.util.function.Consumer
 
 
 @RestController
@@ -16,11 +17,25 @@ import java.util.*
 class ChannelController {
 
     @Autowired
-    lateinit var nameRepository: NameRepository;
+    lateinit var channelRepository: ChannelRepository
 
     @GetMapping("/channels")
-    fun getAllChannels(): MutableList<Name> {
-        nameRepository.save(Name("Test12332"))
-        return nameRepository.findAll();
+    fun getAllChannels(): ResponseEntity<List<Channel>> {
+
+        val result:List<Channel> = prepareResult(channelRepository.findAll())
+        return ResponseEntity<List<Channel>>(result, HttpStatus.OK)
+    }
+
+    private fun prepareResult(channelList: List<Channel>): List<Channel> {
+
+
+        return removeID(channelList);
+    }
+
+    private fun removeID(channelList: List<Channel>) : List<Channel>{
+        channelList.forEach(
+                Consumer { channel -> channel.setId(0)}
+        )
+        return channelList
     }
 }
